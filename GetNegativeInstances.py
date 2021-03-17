7821098d296d926ad9618c609272c081df6933f5
JoseBarbero <jabarbero@ubu.es>
2021-03-17 17:56:00 +0100

    solved bug getting neg instances

--- GetNegativeInstances.py
+++ GetNegativeInstances.py
@@ -13,7 +13,6 @@ We keep those 10 negative instances in a new column of the dataframe as a list. 
 
 
 POS_INSTANCES_DF = 'EveryEnsemblTranscript_withTIS_dataframe.csv'
-#POS_INSTANCES_DF = 'Test_withTIS_dataframe.csv'
 POS_TO_NEG_RATIO = 10   # We generate 10 negative instances for every positive one
 DATAFRAME_OUT_FILE = 'EveryEnsemblTranscript_pos_and_neg.csv'
 
@@ -35,7 +34,7 @@ for idx, row in ensembl_df.iterrows():
     clean_transcript = transcript[transcript_flank_length:-transcript_flank_length]
 
     # Get every occurence of tis_codon in that transcript
-    occurrences_idxs = [i.start() for i in re.finditer('(?='+tis_codon+')', clean_transcript)]
+    occurrences_idxs = [ocurr.start() for ocurr in re.finditer('(?='+tis_codon+')', clean_transcript)]
 
     # Get a random selection of POS_TO_NEG_RATIO occurrences
     # Get 1 less every time there are no enough occurrences    
@@ -52,8 +51,8 @@ for idx, row in ensembl_df.iterrows():
     j = 0
     for neg_idx in neg_idxs:
         idx_with_margin = neg_idx + row['transcriptFlankLength']    # To get the actual coordinate in the flanked transcript
-        ensembl_df.at[idx, f'negative_instance_{i}'] = transcript[idx_with_margin-100:idx_with_margin+103]
-        i+=j
+        ensembl_df.at[idx, f'negative_instance_{j}'] = transcript[idx_with_margin-100:idx_with_margin+103]
+        j+=1
     
     i +=1
     print(f'{i}/{len(ensembl_df)}', end='\r', flush=True)
