c94fa08e85c19a7cdcdb9997010f958cf2172b99
JoseBarbero <jabarbero@ubu.es>
2021-03-19 09:38:19 +0100

    New project structure

---
+++ curate_data/GetNegativeInstances.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import re
+import random
+
+'''
+This script gets a dataframe (created by GetTISFlanks.py) and generates negative ones.
+To simulate negative instances we will select POS_TO_NEG_RATIO random positions in a transcript where the real TIS codon is found.
+
+Usually we find 10 negative instances to every positive one, so this a good value to start with.
+We keep those 10 negative instances in a new column of the dataframe as a list. This is the easiest way to allow filtering these instances by chromosome, gene, etc.
+'''
+
+
+POS_INSTANCES_DF = 'EveryEnsemblTranscript_withTIS_dataframe.csv'
+POS_TO_NEG_RATIO = 10   # We generate 10 negative instances for every positive one
+DATAFRAME_OUT_FILE = 'EveryEnsemblTranscript_pos_and_neg.csv'
+
+
+ensembl_df = pd.read_csv(POS_INSTANCES_DF)
+
+for i in range(POS_TO_NEG_RATIO):
+    ensembl_df[f'negative_instance_{i}'] = ''
+
+
+i=0
+
+for idx, row in ensembl_df.iterrows():
+    tis_flank_length = row['TISFlankLength']
+    tis_codon = row['TIScodon']
+
+    transcript_flank_length = row['transcriptFlankLength']
+    transcript = row['Sequence']
+    clean_transcript = transcript[transcript_flank_length:-transcript_flank_length]
+
+    # Get every occurence of tis_codon in that transcript
+    occurrences_idxs = [ocurr.start() for ocurr in re.finditer('(?='+tis_codon+')', clean_transcript)]
+
+    # Get a random selection of POS_TO_NEG_RATIO occurrences
+    # Get 1 less every time there are no enough occurrences    
+    local_pos_to_neg = POS_TO_NEG_RATIO
+    while True:
+        try:
+            neg_idxs = random.sample(occurrences_idxs, local_pos_to_neg)
+        except ValueError:
+            local_pos_to_neg -= 1
+        else:
+            break
+
+    j = 0
+    for neg_idx in neg_idxs:
+        idx_with_margin = neg_idx + row['transcriptFlankLength']    # To get the actual coordinate in the flanked transcript
+        ensembl_df.at[idx, f'negative_instance_{j}'] = transcript[idx_with_margin-100:idx_with_margin+103]
+        j+=1
+    
+    i +=1
+    print(f'{i}/{len(ensembl_df)}', end='\r', flush=True)
+
+
+ensembl_df.to_csv(DATAFRAME_OUT_FILE)
---
+++ models/LSTMAttention.py
@@ -0,0 +1,118 @@
+import numpy as np
+import re
+import os
+import sys
+import pickle
+from utils.Results import report_results_imagedata, make_spider_by_temp, report_results_st, test_results, plot_train_history
+from datetime import datetime
+from contextlib import redirect_stdout
+import keras
+import tensorflow as tf
+from keras import layers
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
+from keras_self_attention import SeqSelfAttention
+
+
+def lstm_att():
+    sequence_input = tf.keras.layers.Input(shape=(203,4))
+
+    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=64, return_sequences=True, dropout=0.3, input_shape=(200,4)))(sequence_input)
+    x = tf.keras.layers.Attention()([x, x])
+    x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=64, return_sequences=True, dropout=0.3))(x)
+    x = tf.keras.layers.Dropout(0.5)(x)
+    #x = tf.keras.layers.Dense(64)(x)
+    #x = tf.keras.layers.Activation('relu')(x)
+    x = tf.keras.layers.Flatten()(x)
+    output = tf.keras.layers.Dense(1)(x)
+    output = tf.keras.layers.Activation('sigmoid')(output)
+
+    model = tf.keras.Model(inputs=sequence_input, outputs=output)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=["accuracy", 'AUC'])
+
+    return model
+
+if __name__ == "__main__":
+    seed = 42
+    np.random.seed(seed)
+
+    X_train_file = open('../data/onehot_serialized/X_train.pkl', 'rb')
+    y_train_file = open('../data/onehot_serialized/y_train.pkl', 'rb')
+    X_val_file = open('../data/onehot_serialized/X_val.pkl', 'rb')
+    y_val_file = open('../data/onehot_serialized/y_val.pkl', 'rb')
+    X_test_file = open('../data/onehot_serialized/X_test.pkl', 'rb')
+    y_test_file = open('../data/onehot_serialized/y_test.pkl', 'rb')
+
+    X_train = pickle.load(X_train_file)
+    y_train = pickle.load(y_train_file)
+    X_val = pickle.load(X_val_file)
+    y_val = pickle.load(y_val_file)
+    X_test = pickle.load(X_test_file)
+    y_test = pickle.load(y_test_file)
+
+    X_train_file.close()
+    y_train_file.close()
+    X_val_file.close()
+    y_val_file.close()
+    X_test_file.close()
+    y_test_file.close()
+
+#    X_train = X_train[:,:,:4]
+#    X_val = X_val[:,:,:4]
+#    X_test = X_test[:,:,:4]
+
+    print(X_train.shape, y_train.shape)
+    print(X_val.shape, y_val.shape)
+    print(X_test.shape, y_test.shape)
+
+    y_train = y_train.reshape(*y_train.shape, 1)
+    y_val = y_val.reshape(*y_val.shape, 1)
+    y_test = y_test.reshape(*y_test.shape, 1)
+
+    print(X_train.shape, y_train.shape)
+    print(X_val.shape, y_val.shape)
+    print(X_test.shape, y_test.shape)
+
+    if len(sys.argv) < 2:
+        run_id = str(datetime.now()).replace(" ", "_").replace("-", "_").replace(":", "_").split(".")[0]
+    else:
+        run_id = sys.argv[1]
+        #run_id = "".join(categories)
+
+    log_file = "logs/"+run_id+".log"
+    hist_file = "logs/"+run_id+".pkl"
+    plot_file = "logs/"+run_id+".png"
+
+    model = lstm_att()
+    model.build(X_train.shape)
+    model.summary()
+    
+    with open(log_file, 'w') as f:
+        with redirect_stdout(f):
+            model.summary()
+
+            for layer in model.layers:
+                print(layer.get_config())
+            early_stopping_monitor = EarlyStopping( monitor='val_loss', min_delta=0, patience=10, 
+                                                    verbose=1, mode='min', baseline=None,
+                                                    restore_best_weights=True)
+            reduce_lr_loss = ReduceLROnPlateau(monitor='val_auc', factor=0.5, patience=3, verbose=1, min_delta=1e-4, mode='max')
+
+            history = model.fit(X_train, y_train,
+                                shuffle=True,
+                                batch_size=32,
+                                epochs=100,
+                                verbose=True,
+                                validation_data=(X_val, y_val),
+                                callbacks=[early_stopping_monitor, reduce_lr_loss])
+            print("Train results:\n")
+            test_results(X_train, y_train, model)
+            print("Val results:\n")
+            test_results(X_val, y_val, model)
+            print("Test results:\n")
+            test_results(X_test, y_test, model)
+            
+
+    with open(hist_file, 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
+
+    plot_train_history(history.history, plot_file)
