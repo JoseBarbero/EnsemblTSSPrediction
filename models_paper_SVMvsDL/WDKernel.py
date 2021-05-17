0b8c032ee90181f90273767ecaedc00bfe4c77d1
JoseBarbero <jabarbero@ubu.es>
2021-05-17 11:26:22 +0200

    SVM parallelization

--- models_paper_SVMvsDL/WDKernel.py
+++ models_paper_SVMvsDL/WDKernel.py
@@ -10,13 +10,13 @@ def beta_k(d, k):
 
 def fill_per_window(args):
         print(args)
-        window_x, window_y, X1, X2, shared_array, L, d, block_size = args
+        window_x, window_y = args
         tmp = np.ctypeslib.as_array(shared_array)
 
         for idx_x in range(window_x, window_x + block_size):
             for idx_y in range(window_y, window_y + block_size):
-                tmp[idx_x, idx_y] = X1[idx_x, idx_y]
-                tmp[idx_x, idx_y] = get_K_value(X1[idx_x], X2[idx_y], L, d)
+                tmp[idx_x, idx_y] = X_g[idx_x, idx_y]
+                tmp[idx_x, idx_y] = get_K_value(X_g[idx_x], X_g[idx_y], L, d)
 
 
 def parallel_wdkernel_gram_matrix(X1, X2):
@@ -24,14 +24,23 @@ def parallel_wdkernel_gram_matrix(X1, X2):
     
 
     size = X1.shape[0]
-    block_size = 100
-    L = len(X1[0])
     d = 3
+
+    # Needed for multiprocessing
+    global X_g
+    global d_g
+    global L
+    global block_size
+    global shared_array
+    X_g = X1
+    d_g = d
+    L = len(X1[0])
+    block_size = 100
 
     K = np.ctypeslib.as_ctypes(np.zeros((size, size)))
     shared_array = sharedctypes.RawArray(K._type_, K)
 
-    args = [(i, j, X1, X2, shared_array, L, d, block_size) for i, j in itertools.product(range(0, size, block_size), 
+    args = [(i, j) for i, j in itertools.product(range(0, size, block_size), 
                                                         range(0, size, block_size))]
 
     p = Pool()
