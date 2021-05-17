82c6ce47d6cb60bb563409886f46068d64d74d45
JoseBarbero <jabarbero@ubu.es>
2021-05-17 09:12:12 +0200

    SVM test

--- models_paper_SVMvsDL/WDKernel.py
+++ models_paper_SVMvsDL/WDKernel.py
@@ -51,7 +51,6 @@ def get_K_value(xi, xj, L, d):
         E2 = 0
         for l in range(L-k+1):  #Aquí no sumo 1 a cada lado del range porque son posiciones de una lista
             E2 += int(xi[l:l+k] == xj[l:l+k])
-            if E2 > 1:
-                print(E2)
+            print(xi[l:l+k], xj[l:l+k], xi[l:l+k] == xj[l:l+k], int(xi[l:l+k] == xj[l:l+k]))
         E1 += beta * E2
     return E1
