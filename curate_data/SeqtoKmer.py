d41d80c01741a5aabb9650755f2a36cde930c4a2
JoseBarbero <jabarbero@ubu.es>
2021-10-01 13:47:07 +0200

    SeqtoKmer

--- curate_data/SeqtoKmer.py
+++ curate_data/SeqtoKmer.py
@@ -13,10 +13,13 @@ def seq2kmer(seq, k, maxlength=512):
 
     """
     if len(seq) > 512:
+        
         middle_idx = int((len(seq) - 1)/2)
         start_idx = middle_idx-256
         end_idx = middle_idx+256
         seq = seq[start_idx:end_idx]
+
+    print(f"Sequence trimmed to length: {maxlength}")
 
     kmer = [seq[x:x+k] for x in range(len(seq)+1-k)]
     kmers = " ".join(kmer)
