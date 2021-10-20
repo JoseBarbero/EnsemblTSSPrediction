b997a0cc83b0e1bc6fd0e3f677384f70c236503c
JoseBarbero <jabarbero@ubu.es>
2021-10-20 09:17:14 +0200

    bilstm att

--- models_paper_SVMvsDL/biLSTM_Att.py
+++ models_paper_SVMvsDL/biLSTM_Att.py
@@ -18,7 +18,7 @@ from keras.callbacks import LearningRateScheduler
 def bilstm_att():
     sequence_input = tf.keras.layers.Input(shape=(1003,4))
 
-    x = tf.keras.layers.MultiHeadAttention(num_heads=2, key_dim=2, attention_axes=(1))(sequence_input, sequence_input)
+    x = tf.keras.layers.MultiHeadAttention(num_heads=2, key_dim=2, attention_axes=(1,2))(sequence_input, sequence_input)
     x = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(units=128, return_sequences=True))(sequence_input)
     x = tf.keras.layers.Dense(64)(x)
     x = tf.keras.layers.Dropout(0.5)(x)
