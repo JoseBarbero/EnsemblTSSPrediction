988ec012b1c05c0dce799759ae2023fbd5013c51
josebarbero <jabarbero@ubu.es>
2023-02-01 20:13:28 +0100

    Added class distribution to log

--- models_paper_SVMvsDL/DiP_SVM.py
+++ models_paper_SVMvsDL/DiP_SVM.py
@@ -18,126 +18,126 @@ import numpy as np
 from numpy import random
 import pickle 
 
-class CascadeDiPSVM_WD():
-    def __init__(self, fold_size=10000, folds_idxs=[], verbose=True, C=1, kernel="precomputed", degree=3,
-                 gamma="scale", coef0=0.0, probability=True):
-        self.fold_size = fold_size
-        self.folds_idxs = folds_idxs
-        self.verbose = verbose
-        self.C = C
-        self.kernel = kernel
-        self.degree = degree
-        self.gamma = gamma
-        self.coef0 = coef0
-        self.probability = probability
-        base_svc = SVC(C=self.C, kernel=self.kernel, degree=self.degree,
-                       gamma=self.gamma, coef0=self.coef0, probability=self.probability)
-        self.base_svc = base_svc
-        self._estimator_type = "classifier"
+# class CascadeDiPSVM_WD():
+#     def __init__(self, fold_size=10000, folds_idxs=[], verbose=True, C=1, kernel="precomputed", degree=3,
+#                  gamma="scale", coef0=0.0, probability=True):
+#         self.fold_size = fold_size
+#         self.folds_idxs = folds_idxs
+#         self.verbose = verbose
+#         self.C = C
+#         self.kernel = kernel
+#         self.degree = degree
+#         self.gamma = gamma
+#         self.coef0 = coef0
+#         self.probability = probability
+#         base_svc = SVC(C=self.C, kernel=self.kernel, degree=self.degree,
+#                        gamma=self.gamma, coef0=self.coef0, probability=self.probability)
+#         self.base_svc = base_svc
+#         self._estimator_type = "classifier"
 
-    def __get_sv__(self,id,X,y):
-        X_gram_matrix = parallel_wdkernel_gram_matrix(X, X)
-        self.base_svc.fit(X_gram_matrix,y)
-        ind_sv = self.base_svc.support_
-        X = X[ind_sv, :]
-        y = y[ind_sv]
-        id = id[ind_sv]
-        return id, X, y
+#     def __get_sv__(self,id,X,y):
+#         X_gram_matrix = parallel_wdkernel_gram_matrix(X, X)
+#         self.base_svc.fit(X_gram_matrix,y)
+#         ind_sv = self.base_svc.support_
+#         X = X[ind_sv, :]
+#         y = y[ind_sv]
+#         id = id[ind_sv]
+#         return id, X, y
     
-    def __customfold_svc__(self,folds_idxs,X,y):
-        Xsv = []
-        ysv = []
-        idsv = []
-        for fold_idxs in folds_idxs:
-            id_, X_, y_ = self.__get_sv__(fold_idxs,X[fold_idxs],y[fold_idxs])
-            idsv.append(id_)
-            Xsv.append(X_)
-            ysv.append(y_)
-        Xsv = np.vstack(Xsv)
-        ysv = np.concatenate(ysv)
-        idsv = np.concatenate(idsv)
-        return idsv, Xsv, ysv
+#     def __customfold_svc__(self,folds_idxs,X,y):
+#         Xsv = []
+#         ysv = []
+#         idsv = []
+#         for fold_idxs in folds_idxs:
+#             id_, X_, y_ = self.__get_sv__(fold_idxs,X[fold_idxs],y[fold_idxs])
+#             idsv.append(id_)
+#             Xsv.append(X_)
+#             ysv.append(y_)
+#         Xsv = np.vstack(Xsv)
+#         ysv = np.concatenate(ysv)
+#         idsv = np.concatenate(idsv)
+#         return idsv, Xsv, ysv
     
-    def __kfold_svc__(self,id,X,y):
-        skf = StratifiedKFold(n_splits=round(X.shape[0]/self.fold_size),shuffle=True)
-        Xsv = []
-        ysv = []
-        idsv = []
-        for _, ind in skf.split(X,y):
-            id_, X_, y_ = self.__get_sv__(id[ind],X[ind,:],y[ind])
-            print("X_ shape: ", X_.shape)
-            idsv.append(id_)
-            Xsv.append(X_)
-            ysv.append(y_)
-        Xsv = np.vstack(Xsv)
-        ysv = np.concatenate(ysv)
-        idsv = np.concatenate(idsv)
-        return idsv, Xsv, ysv
+#     def __kfold_svc__(self,id,X,y):
+#         skf = StratifiedKFold(n_splits=round(X.shape[0]/self.fold_size),shuffle=True)
+#         Xsv = []
+#         ysv = []
+#         idsv = []
+#         for _, ind in skf.split(X,y):
+#             id_, X_, y_ = self.__get_sv__(id[ind],X[ind,:],y[ind])
+#             print("X_ shape: ", X_.shape)
+#             idsv.append(id_)
+#             Xsv.append(X_)
+#             ysv.append(y_)
+#         Xsv = np.vstack(Xsv)
+#         ysv = np.concatenate(ysv)
+#         idsv = np.concatenate(idsv)
+#         return idsv, Xsv, ysv
     
-    def fit(self,X,y):
-        self.classes_, y = np.unique(y, return_inverse=True)
-        n_init = X.shape[0]
-        k = 1
-        if self.verbose:
-            print("Cascade step "+str(k))
-            print("Total number of instances: "+str(n_init))
+#     def fit(self,X,y):
+#         self.classes_, y = np.unique(y, return_inverse=True)
+#         n_init = X.shape[0]
+#         k = 1
+#         if self.verbose:
+#             print("Cascade step "+str(k))
+#             print("Total number of instances: "+str(n_init))
         
-        id, X, y = self.__customfold_svc__(self.folds_idxs, X, y)
-        n_new = X.shape[0]
-        if self.verbose:
-            print("Number of remaining instances (support vectors): "+str(n_new))
-        print("n_init: ", n_init)
-        print("n_new: ", n_new)
-        print("fold_size: ", self.fold_size)
-        while((n_new>2*self.fold_size)&(((n_init-n_new)/n_init)>0.1)):
-            k = k+10
-            n_init = n_new
-            if self.verbose:
-                print("Cascade step " + str(k))
-            id, X, y = self.__kfold_svc__(id, X, y)
-            n_new = X.shape[0]
-            if self.verbose:
-                print("Number of remaining instances: " + str(n_new))
-                print("n_init: ", n_init)
-                print("n_new: ", n_new)
-                print("fold_size: ", self.fold_size)
-        k = k+1
-        if self.verbose:
-            print("Cascade step " + str(k))
-        last_train = X
-        last_id = id
-        id, X, y = self.__get_sv__(id, X, y)
-        self.nsteps = k
-        self.support_ = id
-        if self.verbose:
-            print("Final number of support vectors: "+str(len(self.support_)))
-        return last_id
+#         id, X, y = self.__customfold_svc__(self.folds_idxs, X, y)
+#         n_new = X.shape[0]
+#         if self.verbose:
+#             print("Number of remaining instances (support vectors): "+str(n_new))
+#         print("n_init: ", n_init)
+#         print("n_new: ", n_new)
+#         print("fold_size: ", self.fold_size)
+#         while((n_new>2*self.fold_size)&(((n_init-n_new)/n_init)>0.1)):
+#             k = k+10
+#             n_init = n_new
+#             if self.verbose:
+#                 print("Cascade step " + str(k))
+#             id, X, y = self.__kfold_svc__(id, X, y)
+#             n_new = X.shape[0]
+#             if self.verbose:
+#                 print("Number of remaining instances: " + str(n_new))
+#                 print("n_init: ", n_init)
+#                 print("n_new: ", n_new)
+#                 print("fold_size: ", self.fold_size)
+#         k = k+1
+#         if self.verbose:
+#             print("Cascade step " + str(k))
+#         last_train = X
+#         last_id = id
+#         id, X, y = self.__get_sv__(id, X, y)
+#         self.nsteps = k
+#         self.support_ = id
+#         if self.verbose:
+#             print("Final number of support vectors: "+str(len(self.support_)))
+#         return last_id
     
-    def decision_function(self,X):
-        return self.base_svc.decision_function(X)
+#     def decision_function(self,X):
+#         return self.base_svc.decision_function(X)
     
-    def predict(self,X):
-        pred = self.classes_[self.base_svc.predict(X)]
-        return pred
+#     def predict(self,X):
+#         pred = self.classes_[self.base_svc.predict(X)]
+#         return pred
     
-    def predict_proba(self,X):
-        prob = self.base_svc.predict_proba(X)
-        return prob
+#     def predict_proba(self,X):
+#         prob = self.base_svc.predict_proba(X)
+#         return prob
     
-    def get_params(self, deep = True):
-        res = {"fold_size" : self.fold_size, "verbose" : self.verbose,
-               "C" : self.C, "kernel" : self.kernel, "degree" : self.degree,
-               "gamma" : self.gamma, "coef0" : self.coef0,
-               "probability" : self.probability}
-        return res
+#     def get_params(self, deep = True):
+#         res = {"fold_size" : self.fold_size, "verbose" : self.verbose,
+#                "C" : self.C, "kernel" : self.kernel, "degree" : self.degree,
+#                "gamma" : self.gamma, "coef0" : self.coef0,
+#                "probability" : self.probability}
+#         return res
     
-    def set_params(self, **parameters):
-        for par, val in parameters.items():
-            if par in ["fold_size","verbose"]:
-                setattr(self, par, val)
-            else:
-                setattr(self.base_svc, par, val)
-        return self
+#     def set_params(self, **parameters):
+#         for par, val in parameters.items():
+#             if par in ["fold_size","verbose"]:
+#                 setattr(self, par, val)
+#             else:
+#                 setattr(self.base_svc, par, val)
+#         return self
 
 def distribution_preserving_partitioning(D, N, C, P, K):
     """
